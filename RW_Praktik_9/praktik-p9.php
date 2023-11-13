a6cacc2ce31a725d9ac854e80b7f541d58ba3341
N Aditiya <94776808+Lucifwr233@users.noreply.github.com>
2023-11-13 13:10:02 +0700

    initial commit
    praktik kolom dalam bootstrap

---
+++ RW_Praktik_9/praktik-p9.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <a>Baris 1</a>
+            </div>
+            <div class="col">
+                <h1>Kolom 1</h1>
+            </div>
+            <div class="col">
+                <h1>Kolom 2</h1>
+            </div>
+            <div class="col">
+                <h1>Kolom 3</h1>
+            </div>
+            <div class="col">
+                <h1>Kolom 4</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <a>Baris 2</a>
+            </div>
+            <div class="col">
+                <h1>Kolom 1</h1>
+            </div>
+            <div class="col">
+                <h1>Kolom 2</h1>
+            </div>
+            <div class="col">
+                <h1>Kolom 3</h1>
+            </div>
+            <div class="col">
+                <h1>Kolom 4</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
