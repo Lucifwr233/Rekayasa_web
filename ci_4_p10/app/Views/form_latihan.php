cf032b052cb796678cac612f8b2584659a2acad0
N Aditiya <94776808+Lucifwr233@users.noreply.github.com>
2023-12-04 13:48:53 +0700

    Update form_latihan.php

--- ci_4_p10/app/Views/form_latihan.php
+++ ci_4_p10/app/Views/form_latihan.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
     <!---- Komponen Tabel Data----->
     <!--- Jquery--->
@@ -21,65 +21,145 @@
     <!-- import google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Flamenco&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Flamenco&display=swap" rel="stylesheet">
 
     <!-- membuat datatables -->
     <script type="text/javascript">
         $(document).ready(function(){$('#tabel_mahasiswa').DataTable();});
     </script>
 </head>
-<body>
-        <h1 class="d-flex justify-content-center"> <?= $judul?> </h1>
-        <h2 class="d-flex justify-content-center"> <?= $sub_judul ?> </h2>
+    <body>
+        <br>
+        <br>
+            <center>
+            <h1 class="d-flex justify-content-center" style="font-family: Flamenco; "> <?= $judul?> </h1>
+            <h2 class="d-flex justify-content-center"> <?= $sub_judul ?> </h2>
+            </center>
 
-    <br>
-    <h1 class=""> <?= $judul_tabel?> </h1>
-    <table class="table table-bordered" id="tabel_mahasiswa">
-        <thead>
-            <th>No.</th>
-            <th>Nama Pegawai</th>
-            <th>Jenis Kelamin</th>
-            <th>Alamat</th>
-        </thead>
-        <tbody>
-            <?php $nomor=1; foreach ($data_pegawai as $pegawai) {?>
-            <tr>
-                <td><?= $nomor?></td>
-                <td><?= $pegawai['nama_pegawai']?></td>
-                <td><?= $pegawai['jenis_kelamin']?></td>
-                <td><?= $pegawai['alamat_pegawai']?></td>
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-    <br>
-    <h1 class=""> <?= $judul_form?> </h1>
-    <!-- form inputan -->
-    <form action="" method="post">
-        <div class="row mb-2">
-            <label class="col-4" for="">Nama Pegawai</label>
-                <div class="col-8">
-                <input type="text" name="input_nama" class="form-control">
+        <br>
+        <br>
+        <br>
+            <div class="container">
+            <div class="row">
+                <div class="col-6">
+                        <div class="card-header">
+                            <h1 class="" style="font-family: Bebas Neue; "> <?= $judul_tabel?> </h1>
+                        </div>
+                        <table class="table table-bordered" id="tabel_mahasiswa">
+                            <thead>
+                                <th>No.</th>
+                                <th>Nama Pegawai</th>
+                                <th>Jenis Kelamin</th>
+                                <th>Alamat</th>
+                            </thead>
+                            <tbody>
+                                <?php $nomor=1; foreach ($data_pegawai as $pegawai) {?>
+                                <tr>
+                                    <td><?= $nomor?></td>
+                                    <td><?= $pegawai['nama_pegawai']?></td>
+                                    <td><?= $pegawai['jenis_kelamin']?></td>
+                                    <td><?= $pegawai['alamat_pegawai']?></td>
+                                </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                <br>
+                <div class="col-6">
+                    <div class="card-header">
+                        <h1 class="" style="font-family: Bebas Neue; "> <?= $judul_form?> </h1>
+                    </div>
+                        <!-- form inputan -->
+                        <form action="" method="post">
+                            <div class="row mb-2">
+                                <label class="col-4" for="">Nama Pegawai</label>
+                                    <div class="col-8">
+                                    <input type="text" name="input_nama" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label class="col-4" for="">Jenis Kelamin</label>
+                                <div class="col-8">
+                                    <select name="input_jk" id="" class="form-control">
+                                    <option value="Pria">Pria</option>
+                                    <option value="Wanita">Wanita</option>
+                                </select>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label class="col-4" for="">Alamat</label>
+                                <div class="col-8">
+                                    <textarea name="input_alamat" id="" class="form-control"></textarea>
+                                </div>
+                            </div>
+                            <br>
+                            <!-- button -->
+                            <button class="btn btn-success btn-lg btn-block" type="button"><i class="fa fa-save"></i> Simpan </button>
+                            <a href="<?= base_url('Latihan');?>" class="btn btn-danger btn-block  btn-lg"><i class="fa fa-refresh fa-spin"></i> Muat Ulang</a>
+                        </form>
+                </div>
             </div>
+            <br>
+            <br>
+            <br>
+            <br>
+            <center>
+                <div class="navbar-tool" id="dark-mode-toggle">
+                    <i class="fas fa-sun" id="dark-mode-icon"></i>
+                </div>
+            </center>
         </div>
-        <div class="row">
-            <label class="col-4" for="">Jenis Kelamin</label>
-            <div class="col-8">
-                <select name="input_jk" id="" class="form-control">
-                <option value="Pria">Pria</option>
-                <option value="Wanita">Wanita</option>
-            </select>
-            </div>
-        </div>
-        <div class="row">
-            <label class="col-4" for="">Alamat</label>
-            <div class="col-8">
-                <textarea name="input_alamat" id="" class="form-control"></textarea>
-            </div>
-        </div>
-        <!-- button -->
-        <button class="btn btn-success btn-lg btn-block" type="button"><i class="fa fa-save"></i> Simpan </button>
-        <a href="<?= base_url('Latihan');?>" class="btn btn-danger btn-block  btn-lg"><i class="fa fa-refresh fa-spin"></i> Muat Ulang</a>
-    </form>
-</body>
+    </body>
+
+    <script type="text/javascript">
+        function myFunction() {
+            var element = document.body;
+            element.classList.toggle("dark-mode");
+        }
+
+        const darkModeToggle = document.getElementById("dark-mode-toggle");
+        const darkModeIcon = document.getElementById("dark-mode-icon");
+        const body = document.body;
+        let isDarkMode = false;
+
+        darkModeToggle.addEventListener("click", () => {
+            isDarkMode = !isDarkMode;
+            body.classList.toggle("dark-mode", isDarkMode);
+            updateDarkModeIcon();
+            animateDarkModeToggle();
+        });
+
+        function updateDarkModeIcon() {
+        const darkModeIcon = document.getElementById("dark-mode-icon");
+
+        if (isDarkMode) {
+            darkModeIcon.classList.remove("fa-moon");
+            darkModeIcon.classList.add("fa-sun");
+        } else {
+            darkModeIcon.classList.remove("fa-sun");
+            darkModeIcon.classList.add("fa-moon");
+        }
+    }
+
+        function toggleDarkMode() {
+            isDarkMode = !isDarkMode;
+
+            if (isDarkMode) {
+                document.querySelector('link[data-bs-theme]').removeAttribute('data-bs-theme');
+                document.querySelector('link[href*="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"]').removeAttribute('sdata-bs-theme');
+            } else {
+                document.querySelector('link[href*="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"]').setAttribute('data-bs-theme', 'dark');
+            }
+
+            updateDarkModeIcon();
+        }
+
+        function animateDarkModeToggle() {
+            darkModeToggle.classList.add("animate");
+            setTimeout(() => {
+                darkModeToggle.classList.remove("animate");
+            }, 1000);
+        }
+
+    </script>
 </html>
