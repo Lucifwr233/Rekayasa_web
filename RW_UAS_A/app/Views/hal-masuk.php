e2669ca7e09b3866ce8634654bd1d682099839ff
N Aditiya <94776808+Lucifwr233@users.noreply.github.com>
2024-01-09 17:54:02 +0700

    Update hal-masuk.php

--- RW_UAS_A/app/Views/hal-masuk.php
+++ RW_UAS_A/app/Views/hal-masuk.php
@@ -163,7 +163,6 @@
                 <img src="https://images.pexels.com/photos/2033997/pexels-photo-2033997.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
                     class="" alt="">
             </div>
-            
             <div class=" box-2 d-flex flex-column h-100">
                 <div class="mt-5">
                     <p class="mb-1 h-1">Login Account.</p>
