4353c98908c63ecd2b8f427163db21a3981ddc88
N Aditiya <94776808+Lucifwr233@users.noreply.github.com>
2024-01-12 16:32:36 +0700

    Update welcome_message.php

--- RW_UAS_A/app/Views/welcome_message.php
+++ RW_UAS_A/app/Views/welcome_message.php
@@ -158,13 +158,12 @@
 			color: rgba(255, 255, 255, 1);
 			padding: 2rem 1.75rem;
 		}
-		
 		footer .copyrights {
 			background-color: rgba(62, 62, 62, 1);
 			color: rgba(200, 200, 200, 1);
 			padding: .25rem 1.75rem;
 		}
-
+		
 		@media (max-width: 559px) {
 			header ul {
 				padding: 0;
