c9bc3d1eb3cf0f289af55deedee1ddf2668139f0
N Aditiya <94776808+Lucifwr233@users.noreply.github.com>
2024-01-14 18:21:31 +0700

    Update welcome_message.php

--- RW_UAS_A/app/Views/welcome_message.php
+++ RW_UAS_A/app/Views/welcome_message.php
@@ -108,12 +108,13 @@
 			font-size: 1.5rem;
 			font-weight: 300;
 		}
+		
 		section {
 			margin: 0 auto;
 			max-width: 1100px;
 			padding: 2.5rem 1.75rem 3.5rem 1.75rem;
 		}
-		
+
 		section h1 {
 			margin-bottom: 2.5rem;
 		}
